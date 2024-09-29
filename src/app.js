07a87ac1a14f7e71864f76c293d71714390653a8
Christian Saavedra <chrisosami86@gmail.com>
2024-09-28 21:08:52 -0500

    Nueva pagina de aviso de bonos agotados

--- src/app.js
+++ src/app.js
@@ -1,35 +1,34 @@
-import  htmlForm from "./formularioHTML.html?raw";
+import htmlForm from "./formularioHTML.html?raw";
+import htmlMensaje from "./mensajeHTML.html?raw";
 
-export const App = (elementId) => {    
-
-
-    (()=>{
-
+export const App = (elementId) => {
+    (async () => {
         const app = document.createElement('div');
-        app.innerHTML = htmlForm; //mandando la importacion de html en crudo
-        document.querySelector(elementId).append(app);
-        document.getElementById('fecha-hora').value = new Date().toLocaleString();
 
+        // Verificar la cantidad de bonos disponibles
+        const bonosResponse = await fetch('https://back-bonos.vercel.app/bonos');
+        const bonosData = await bonosResponse.json();
 
-        document.getElementById('registro-form').addEventListener('submit', async (event) => {
-            event.preventDefault(); // Evita el envío predeterminado del formulario
-        
-            // Recoger los datos del formulario
-            const formData = {
-                fechaHora: document.getElementById('fecha-hora').value,
-                correo: document.getElementById('correo').value,
-                codigoEstudiante: document.getElementById('codigo-estudiante').value,
-                numeroIdentificacion: document.getElementById('numero-identificacion').value,
-                programaAcademico: document.getElementById('programa-academico').value,
-                recibo: document.querySelector('input[name="recibo"]:checked').value,
-            };
-        
-            try {
-                // Verificar la cantidad de bonos disponibles
-                const bonosResponse = await fetch('https://back-bonos.vercel.app/bonos');
-                const bonosData = await bonosResponse.json();
-        
-                if (bonosData.bonosDisponibles > 0) {
+        if (bonosData.bonosDisponibles > 0) {
+            // Renderizar el formulario si hay bonos
+            app.innerHTML = htmlForm; // mandando la importacion de html en crudo
+            document.querySelector(elementId).append(app);
+            document.getElementById('fecha-hora').value = new Date().toLocaleString();
+
+            document.getElementById('registro-form').addEventListener('submit', async (event) => {
+                event.preventDefault(); // Evita el envío predeterminado del formulario
+
+                // Recoger los datos del formulario
+                const formData = {
+                    fechaHora: document.getElementById('fecha-hora').value,
+                    correo: document.getElementById('correo').value,
+                    codigoEstudiante: document.getElementById('codigo-estudiante').value,
+                    numeroIdentificacion: document.getElementById('numero-identificacion').value,
+                    programaAcademico: document.getElementById('programa-academico').value,
+                    recibo: document.querySelector('input[name="recibo"]:checked').value,
+                };
+
+                try {
                     // Enviar los datos al servidor
                     const response = await fetch('https://back-bonos.vercel.app/bonos', {
                         method: 'PUT',
@@ -46,26 +45,26 @@ export const App = (elementId) => {
                             recibo: formData.recibo,
                         }),
                     });
-        
+
                     if (response.ok) {
                         alert('Bono registrado exitosamente.');
                         // Limpiar el formulario
                         document.getElementById('registro-form').reset();
                         document.getElementById('fecha-hora').value = new Date().toLocaleString();
+                        location.reload();
                     } else {
                         alert('Error al registrar el bono.');
                     }
-                } else {
-                    alert('No hay bonos disponibles.');
-                    // Limpiar el formulario
-                    document.getElementById('registro-form').reset();
-                    document.getElementById('fecha-hora').value = new Date().toLocaleString();
+                } catch (error) {
+                    console.error('Error:', error);
+                    alert('Se produjo un error al procesar tu solicitud.');
                 }
-            } catch (error) {
-                console.error('Error:', error);
-                alert('Se produjo un error al procesar tu solicitud.');
-            }
-        });
-
+            });
+        } else {
+            // Renderizar mensaje de agotamiento de bonos
+            const mensajeAgotado = document.createElement('div');
+            mensajeAgotado.innerHTML = htmlMensaje;
+            document.querySelector(elementId).append(mensajeAgotado);
+        }
     })();
-}
+};
